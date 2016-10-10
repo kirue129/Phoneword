9dedd01593b3ad721c0d15cae7a4bfedcfd6a09c
kirue129 <kirue129.develop@gmail.com>
2016-10-10 16:20:08 +0900

    ロジックコードの追加

--- Phoneword/Phoneword/MainPage1.xaml.cs
+++ Phoneword/Phoneword/MainPage1.xaml.cs
@@ -10,9 +10,38 @@ namespace Phoneword
 {
     public partial class MainPage1 : ContentPage
     {
+
+        string translatedNumber;
+
         public MainPage1()
         {
             InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert("Dial a Number", "Would you like to call " + translatedNumber + "?", "Yes", "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                {
+                    dialer.Dial(translatedNumber);
+                }  
+            }
         }
     }
 }
